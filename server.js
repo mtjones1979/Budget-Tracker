b9facf5176950094a3a684512db801c79ee2e504
Michael Jones <mtjones1979@yahoo.com>
2021-05-02 15:05:31 -0500

    updated server.js with process.env.MONGODB_URI

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.mongodb || "mongodb://localhost/budget", {
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/budget", 
+{
   useNewUrlParser: true,
   useFindAndModify: false
 });
